6cd10bc2cd9a4c106eaf7072b29e2fb92728a50f
Niloufa-Feyz <nilouton@gmail.com>
2024-10-31 10:37:03 +0330

    connecting to backend

--- frist-app/second-app/src/App.tsx
+++ frist-app/second-app/src/App.tsx
@@ -1,15 +1,74 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+interface user {
+  id: number;
+  name: string;
+  email: string;
+}
 function App() {
-  const [users, setUsers] = useState([]);
-
+  const [users, setUsers] = useState<user[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true);
     axios
-      .get("https://jsonplaceholder.typicode.com/users")
-      .then((res) => console.log(res));
-  });
-  return <div></div>;
+      .get<user[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  const deleteUser = (user: user) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+  };
+  axios
+    .delete('https://jsonplaceholder.typicode.com/users' + 'user.id')
+    .catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  return (
+    <div>
+      {isLoading && <div className="spinner-border"></div>}
+      {error && <p className="text-danger">{error}</p>}
+      <h2>Users Name</h2>
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}{" "}
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => deleteUser(user)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+      <h2>Users Emails</h2>
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            key={user.email}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.email}
+            <button className="btn btn-outline-danger">Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
